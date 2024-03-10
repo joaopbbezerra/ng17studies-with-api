6a8fa9afbc30b5d7f405e5e5a049bc9fabc4f2ac
João Pedro Bezerra <joaopbbezerra@gmail.com>
2024-03-10 23:08:30 +0000

    feature: Style improvements

--- src/app/home/ui/gif-list/gif-list.component.ts
+++ src/app/home/ui/gif-list/gif-list.component.ts
@@ -1,16 +1,25 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import {Gif} from "../../../shared/interfaces";
 import {GifPlayerComponent} from "../gif-player/gif-player.component";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {WINDOW} from "../../../shared/utils/injection-token";
 
 @Component({
   selector: 'app-gif-list',
   standalone: true,
   imports: [
-    GifPlayerComponent
+    GifPlayerComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   templateUrl: './gif-list.component.html',
   styleUrl: './gif-list.component.scss'
 })
 export class GifListComponent {
   @Input({required: true}) gifs!: Gif[]
+
+  window = inject(WINDOW);
 }
