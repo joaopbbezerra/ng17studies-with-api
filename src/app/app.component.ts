07625774d2a3e845dd3194127f5de24d335d8eb5
João Pedro Bezerra <joaopbbezerra@gmail.com>
2024-03-10 15:34:40 +0000

    ng basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  private http = inject(HttpClient)
   title = 'angular-api';
+
+  ngOnInit() {
+    this.http.get('http://localhost:3000/').subscribe(res => console.log(res))
+
+    const guess = { guess: 14 };
+
+    this.http.post('http://localhost:3000/', guess).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
